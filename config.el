44d0a5779c912c51d8cbbfea26e65aa706700a0d
Wei Ma <wei.ma@sage.com>
2020-05-01 13:43:40 -0700

    Added more config

--- config.el
+++ config.el
@@ -35,8 +35,8 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
-(setq org-directory "~/work/src-work-notes")
+;; (setq org-directory "~/org/")
+(setq org-directory "~/work/src/work-notes")
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
